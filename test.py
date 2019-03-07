c0212918bb2e0a3575e8179fb1135a5bfc61e0af
George-Ashworth <georgepashworth@gmail.com>
2019-03-07 02:29:17 +0000

    test

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print ("hello world")
+try:
+    import simplegui
+except ImportError:
+    import SimpleGUICS2Pygame as simplegui
+
+message = "Welcome!"
+
+
+def click():
+    global message
+    message = "Good job!"
+
+
+def draw(canvas):
+    canvas.draw_text(message, [50, 112], 48, "Red")
+
+
+frame = simplegui.create_frame("Home", 300, 200)
+frame.add_button("Click me", click)
+frame.set_draw_handler(draw)
+
+frame.start()

2142fa251510143bd9ea95e8ca0fa377c8cec6d0
TrollheimII <dylan.trollope@gmail.com>
2019-03-19 17:34:59 +0000

    Added a chasing wall TODO: collide player with obstacle

--- SideScroller.py
+++ SideScroller.py
@@ -11,6 +11,7 @@
 
 from Vector import Vector
 import random
+import math
 
 try:
     import simplegui
@@ -24,23 +25,53 @@ SPEED = 3
 GRAVITY = 0.020
 
 
+class Chaser:
+
+    def __init__(self):
+        self.pos = Vector()
+        self.p1 = 0
+        self.p2 = 0
+        self.p3 = 0
+        self.p4 = 0
+        self.vel = Vector(SPEED * 0.8, 0)
+        self.height = HEIGHT
+        self.width = 50
+
+    def draw(self, canvas):
+        p1 = self.pos  # bottom left
+        p2 = self.pos + Vector(0, + self.height)  # topleft
+        p3 = self.pos + Vector(self.width, 0)  # bottom right
+        p4 = self.pos + Vector(self.width, + self.height)  # top right
+        canvas.draw_polygon([p1.get_p(), p2.get_p(), p4.get_p(), p3.get_p()], 5, 'Red', 'Red')
+
+    def move_chaser(self, distance):
+        self.p3 = self.p3 + distance
+        self.p4 = self.p4 + distance
+
 
 class Player:
 
     def __init__(self):  # init the player of w x h halfway up the screen
-        self.pos = Vector(50, HEIGHT / 2)
+        self.pos = Vector(75, HEIGHT / 2)
+        self.score = 0
         self.vel = Vector()
         self.height = 40
         self.width = 20
         self.gravity = Vector(0, 9.8)
+
 
     def draw(self, canvas):  # draw the player
         p1 = self.pos  # bottom left
         p2 = self.pos + Vector(0, -self.height)  # topleft
         p3 = self.pos + Vector(self.width, 0)  # bottom right
         p4 = self.pos + Vector(self.width, -self.height)  # top right
-
         canvas.draw_polygon([p1.get_p(), p2.get_p(), p4.get_p(), p3.get_p()], 5, 'Yellow', 'Yellow')
+
+
+    def draw_score(self, canvas):
+        global WIDTH
+        canvas.draw_text(str(self.score), (WIDTH - 100, 20), 12, 'White')
+
 
     def movePlayer(self, yVal):  # this moves the player in the y direction
         global GRAVITY
@@ -48,7 +79,12 @@ class Player:
         self.pos.add(self.vel)
 
 
-class KeyHandler: # when the player presses space, the character jumps
+    def increment_score(self, speed):
+        self.score += speed * 1.5
+        self.score = math.floor(self.score)
+
+
+class KeyHandler:  # when the player presses space, the character jumps
 
     def __init__(self):
         self.space = False
@@ -58,7 +94,6 @@ class KeyHandler: # when the player presses space, the character jumps
             self.space = True
 
 
-
 class Obstacle:
 
     def __init__(self, ob_type, parent_length):
@@ -66,6 +101,7 @@ class Obstacle:
         self.height = 20
         self.width = 20
         self.pos = Vector(random.randrange(self.width, parent_length - self.width), 0)
+
 
     def draw(self, canvas, pos):
         x1 = self.pos + pos
@@ -114,7 +150,21 @@ class Floor:
         self.pos.subtract(Vector(SPEED, 0))
 
 
-class Interaction:
+class ObstacleInteraction:
+
+    def __init__(self, player, other):
+        self.player = player
+        self.other = other
+        self.inCollision = False
+
+    def update(self):
+        # if player hits an object
+        if self.player.pos.x == self.other.pos.x and self.player.pos.y == self.other.pos.y:
+            self.inCollision = True
+            print("colliding with obstacle")
+
+
+class FloorInteraction:
 
     def __init__(self, player, other):
 
@@ -132,12 +182,9 @@ class Interaction:
 
         elif self.off_platform_x():
             if self.falling():
-                if self.collide_left_wall():
-                    print("left wall")
-                else:
-                    self.inCollision = False
-                    print("game over")
-                    frame.stop()
+                self.inCollision = False
+                print("game over")
+                frame.stop()
 
         return self.inCollision
 
@@ -148,7 +195,7 @@ class Interaction:
         return (self.player.pos.x < self.other.pos.x) or self.player.pos.x > self.other.pos.x + self.other.length
 
     def on_platform_x(self):
-        return (self.player.pos.x + self.player.width >= self.other.pos.x) & (self.player.pos.x      <= self.other.pos.x + self.other.length)
+        return (self.player.pos.x + self.player.width >= self.other.pos.x) & (self.player.pos.x <= self.other.pos.x + self.other.length)
 
     def falling(self):
         return self.player.pos.y - self.player.height - self.player.height > self.other.pos.y
@@ -166,33 +213,47 @@ class SideScroller:
         self.floors = []
         self.floors.append(Floor(True))
         self.p = Player()
+        self.c = Chaser()
+
 
     def draw(self, canvas):
-        maximum = len(self.floors)
+        global SPEED
+        max_floor = len(self.floors)
         i = 0
-        inter = Interaction(self.p, self.floors[i ])
+        j = 0
 
-        if inter.update(): #if its colliding
-            if kbd.space:
-                self.p.movePlayer(-2.5)
-                kbd.space = False
+        inter_floor = FloorInteraction(self.p, self.floors[i])
 
-
-        while i < maximum:
+        while i < max_floor:
             if (i == 0) & (self.floors[i].expire_check()):
                 self.floors.pop(0)
                 i = i - 1
             else:
-                if (i == maximum - 1) & (self.floors[i].create_check()):
+                if (i == max_floor - 1) & (self.floors[i].create_check()):
                     self.floors.append(Floor())
 
-                self.floors[i].draw(canvas)
-                self.p.draw(canvas)
-                inter.update()
-                self.p.movePlayer(GRAVITY)
+            if inter_floor.update():  # if its colliding
+                if kbd.space:
+                    self.p.movePlayer(-2.5)
+                    kbd.space = False
 
-            maximum = len(self.floors)
+            for o in self.floors[i].obstacles:
+                inter_obs = ObstacleInteraction(self.p, o)
+
+                inter_obs.update()
+
+            inter_floor.update()
+            self.c.move_chaser(5)
+            self.p.movePlayer(GRAVITY)
+            self.p.increment_score(SPEED)
+            self.p.draw_score(canvas)
+            self.c.draw(canvas)
+            self.p.draw(canvas)
+            self.floors[i].draw(canvas)
+
+            max_floor = len(self.floors)
             i = i + 1
+
 
 
 ss = SideScroller()
